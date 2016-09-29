6431eed8b498f7cebc8cf66bf2ce067f555412fd
Carlos Casares <ccasares@gmail.com>
2016-09-29 15:11:14 +0200

    mapping

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ router.post(LAP, function(req, res) {
   var input = req.body;
   var data = {
     deviceid: input.deviceId,
-    datetime: new Date(input.dateTime * 1000),
+    datetime: new Date(input.dateTime),
     datetimestring: input.dateTimeString,
     racestatus: input.raceStatus,
     raceid: input.raceId,
